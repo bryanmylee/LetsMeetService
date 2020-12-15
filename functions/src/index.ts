472861be3468bfbb8ae451cb0c49fd793d474517
Bryan Lee <bryanmylee@gmail.com>
2020-12-15 15:39:43 +0800

    style: simplify index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,23 @@
 import * as functions from 'firebase-functions';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 
 import AuthService from './service/AuthService';
 import EventRepo from './database/EventRepo';
-import { applyPreMiddlewares } from './middlewares';
 import { errorHandler } from './middleware/ErrorHandler';
 import { login, logout, issueNewAccess } from './route/Auth';
 import { newEvent, getEvent } from './route/Event';
 import { newUser, editUser } from './route/User';
 
 const app = express();
-applyPreMiddlewares(app);
+app.use(cookieParser());
+app.use((req, res, next) => {
+  next();
+}, cors({
+  origin: functions.config().api.client_host,
+  credentials: true,
+}));
 
 const eventRepo = new EventRepo();
 const authService = new AuthService(eventRepo);
